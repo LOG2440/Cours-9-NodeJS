b217304d3e1cf9abaaf42fede7428874d27ad3c5
kegaub <kevin.gauthier.22.06@gmail.com>
2021-10-26 12:15:33 -0400

    Update de status code dans 2 exemples

--- nodejs_avec_express/traitement_requête_reponse/exemple_simple.js
+++ nodejs_avec_express/traitement_requête_reponse/exemple_simple.js
@@ -8,9 +8,10 @@ app.use(express.json())
 app.post("/home/:id", (req, res, next) => {
     console.log(req.params.id)
     console.log(req.body)
+    console.log(req.query)
     res.cookie("id", req.params.id)
     res.set('Content-Type', 'text/plain')
-    res.status(301).send(req.body)
+    res.status(200).send(req.body)
 })
    
 http.createServer(app).listen(3000)
