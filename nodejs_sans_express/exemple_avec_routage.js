16d51fc5d8e6d175b2ed7a8fb3b039a50ae2a1f6
Nikolay Radoev <nikolay.radoev@polymtl.ca>
2021-10-26 11:17:09 -0400

    Native NodeJS routing example

--- nodejs_sans_express/exemple_avec_routage.js
+++ nodejs_sans_express/exemple_avec_routage.js
@@ -1,21 +1,30 @@
-let http = require('http');
-let url = require('url');
+let http = require("http");
+let url = require("url");
 
-http.createServer(function(req,res){
+http
+  .createServer(function (req, res) {
     // On décompose la requête
-    let r = url.parse(req.url,true);
+    let r = url.parse(req.url, true);
     let path = r.pathname;
     let query = r.query;
-
-    switch(path) {
-    case '/':
-        res.writeHead(200, {'Content-Type': 'text/plain;' });
-        res.end('Page principale');
+    switch (path) {
+      case "/":
+        res.writeHead(200, { "Content-Type": "text/plain;" });
+        res.end("Page principale");
         break;
-    default:
-        res.writeHead(404, { 'Content-Type': 'text/plain; charset=utf-8' });
-        res.end('Désolé. Cette page n\'existe pas');
+      case "/about":
+        res.writeHead(200, { "Content-Type": "text/plain;" });
+        res.end("Page d'information");
+        break;
+      case "/data":
+        res.writeHead(200, { "Content-Type": "text/plain;" });
+        const data = query.message ? query.message : "rien";
+        res.end(`Vous avez envoye : ${data}`);
+        break;
+      default:
+        res.writeHead(404, { "Content-Type": "text/plain; charset=utf-8" });
+        res.end("Désolé. Cette page n'existe pas");
         break;
     }
-}).listen(3000);
-   
+  })
+  .listen(3000);
