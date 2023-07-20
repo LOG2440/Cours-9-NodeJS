85aa3c509022caca5bcbab66dd0065595bd550f1
Nikolay Radoev <nikolay.radoev@polymtl.ca>
2023-07-20 14:13:27 -0400

    Refactor routes examples

--- nodejs_express/routing/routes.js
+++ nodejs_express/routing/routes.js
@@ -1,5 +1,4 @@
-let express = require("express");
-let router = express.Router();
+const router = require("express").Router();
 
 /* GET home page. */
 router.get("/", (req, res, next) => {
@@ -7,11 +6,12 @@ router.get("/", (req, res, next) => {
 });
 router.get("/about", (req, res, next) => {
   res.send("Page à propos");
-  next();
 });
 
 router.get("/:id", (req, res, next) => {
-  console.log(`Vous avez envoyé : ${req.params.id}`);
+  const message = `Vous avez envoyé : ${req.params.id}`;
+  console.log(message);
+  res.send(message);
 });
 
 module.exports = router;
--- nodejs_express/variables/server.js
+++ nodejs_express/variables/server.js
@@ -1,7 +1,6 @@
-let express = require("express");
-let http = require("http");
-
-let app = express();
+const express = require("express");
+const http = require("http");
+const app = express();
 
 // On inclut nos middlware ici
 app.use(express.json());
@@ -12,8 +11,8 @@ app.post("/home/:id", (req, res, next) => {
   console.log(req.query);
   res.cookie("id", req.params.id);
   res.set("Content-Type", "application/json");
-  // res.status(200).send(req.body);
-  res.status(200).send({...req.body, random : Math.random()})
+  res.status(201).send({...req.body, random : Math.random()});
 });
 
 http.createServer(app).listen(3000);
+console.log('Serveur avec Express démarré sur localhost:3000');
