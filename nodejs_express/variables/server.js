f409d85f35979e255f0312622aed528dbff433cb
Nikolay Radoev <nikolay.radoev@polymtl.ca>
2024-03-14 22:03:38 -0400

    Added example for request get header

--- nodejs_express/variables/server.js
+++ nodejs_express/variables/server.js
@@ -9,6 +9,7 @@ app.post("/home/:id", (req, res, next) => {
   console.log(`query x : ${req.query.x}`);
   console.log(`request body :`, req.body);
   console.log(`request query :`, req.query);
+  console.log(`user agent header : `, req.get('user-agent'));
   res.cookie("id", req.params.id);
   res.set("Content-Type", "application/json");
   res.status(201).send({ ...req.body, random: Math.random() });
